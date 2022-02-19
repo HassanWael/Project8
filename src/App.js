558aadd346e0c88b907986712a0d95af00881043
QusayAL-Sherbi <qusay.alsherbi@gmail.com>
2022-02-19 20:24:21 +0200

    hero

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css'
-import LogInForm from './components/Login/RegisterForm'
-import RegisterForm from './components/Login/LoginForm'
-import Nav from './components/NavBar/navbar'
-import Footer from './components/footer/footer'
+import LogInForm from './Components/Login/LoginForm'
+import RegisterForm from './Components/Login/RegisterForm'
+import Nav from './Components/navbar/navbar'
+import Footer from './Components/footer/footer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Booking from './components/Booking/Booking'
+import Booking from './Components/Booking/Booking'
 function App() {
   return (
     <>
       <BrowserRouter>
         <Nav />
         <Routes>
-        <Route path='/login' element= {<LogInForm />}></Route>  
-        <Route path='/signup' element= {<RegisterForm/>}></Route>  
-        <Route path='/Book' element= {<Booking/>}></Route>  
+          <Route path='/login' element= {<LogInForm />}></Route>  
+          <Route path='/signup' element= {<RegisterForm/>}></Route>  
+          <Route path='/Book' element= {<Booking/>}></Route>  
         </Routes>
         <Footer />
       </BrowserRouter>
