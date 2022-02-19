dbda4e9d20ad76f2c170921a31e8fbad70311b84
Hassan Wael <hassanwael1999@gmail.com>
2022-02-19 18:38:10 +0200

    mearge

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-import './App.css';
-import RegisterForm from './components/Login/RegisterForm';
-import LogInForm from './components/Login/LoginForm';
-import Nav from './components/NavBar/navbar'
-function App() {
-  return (
-  <>
-  <Nav></Nav>
- 
-  <RegisterForm/>
-  </>
-=======
 import './App.css'
 import LogInForm from './components/Login/RegisterForm'
 import RegisterForm from './components/Login/LoginForm'
-import Nav from './components/navbar/navbar'
+import Nav from './components/NavBar/navbar'
 import Footer from './components/footer/footer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Booking from './components/Booking/Booking'
@@ -31,7 +18,6 @@ function App() {
         <Footer />
       </BrowserRouter>
     </>
->>>>>>> 728379c17ca58f1f0d644c883db427bdb36c6741
   )
 }
 export default App
---
+++ src/components/LandingPage/AboutUs.jsx
@@ -0,0 +1,12 @@
+function AboutUs() {
+    return ( 
+        <div className="container">
+<div><h2> <span>01.</span> About me</h2></div>
+<p>
+    Hello!
+</p>
+        </div>
+     );
+}
+
+export default AboutUs ;
